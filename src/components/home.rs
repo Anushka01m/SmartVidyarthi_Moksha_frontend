1489986c98a7b4c27c510cc1a5f76a68aaff9b8c
Anushka Maitra <anushka.mukti3011@gmail.com>
2025-09-28 11:09:22 +0530

    Create home.rs

---
+++ src/components/home.rs
@@ -0,0 +1,10 @@
+use leptos::*;
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <div class="kid-card">
+            <h1>"Welcome to Rural Learning!"</h1>
+            <p>"Login or Register to begin."</p>
+        </div>
+    }
+}
